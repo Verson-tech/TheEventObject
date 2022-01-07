9552dd636f0b8740990721244715dce7155f050c
vira.murray <vira.avramenko@gmail.com>
2022-01-07 12:58:46 +0200

    readme

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Keyboard Events & Event Objects
 
 Arrow key events:
 ![dev tools console](./Capture1.PNG)
+
+Assigning only specific keys (while the rest is being ignmored):
+![dev tools console](./Capture2.PNG)
--- app.js
+++ app.js
@@ -12,24 +12,20 @@ document.querySelector('button').addEventListener('click', function (evt) {
 // })
 
 window.addEventListener('keydown', function (e) {
-    console.log(e.code)
+    switch (e.code) {
+        case 'ArrowUp':
+            console.log("UP!");
+            break;
+        case 'ArrowDown':
+            console.log("DOWN!");
+            break;
+        case 'ArrowLeft':
+            console.log("LEFT!");
+            break;
+        case 'ArrowRight':
+            console.log("RIGHT!");
+            break
+        default:
+            console.log("IGNORED!")
+    }
 })
-
-
-//     switch (e.code) {
-//         case 'ArrowUp':
-//             console.log("UP!");
-//             break;
-//         case 'ArrowDown':
-//             console.log("DOWN!");
-//             break;
-//         case 'ArrowLeft':
-//             console.log("LEFT!");
-//             break;
-//         case 'ArrowRight':
-//             console.log("RIGHT!");
-//             break
-//         default:
-//             console.log("IGNORED!")
-//     }
-// })
